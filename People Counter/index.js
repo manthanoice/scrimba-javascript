a7076779960a250eca5492dcd6dc92b5b76af9da
Manthanoice <mvbosamiya20@gmail.com>
2022-09-01 11:58:02 +0530

    add

--- People Counter/index.js
+++ People Counter/index.js
@@ -1,6 +1,18 @@
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
 let count = 0
+
+console.log(saveEl)
+
 function increment(){
     count ++
     countEl.innerText = count
 }
+
+function save(){
+    let the_count = count + " - "
+    saveEl.innerText += the_count
+    console.log(count)
+}
+
+save()
